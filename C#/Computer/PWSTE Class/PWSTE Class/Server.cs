c3847caf83e0f9fcc7afdc0cdd977db24c8a981f
Yuumei-hub <74659766+Yuumei-hub@users.noreply.github.com>
2022-11-18 17:58:24 +0100

    servers has been added

---
+++ C#/Computer/PWSTE Class/PWSTE Class/Server.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PWSTE_Class
+{
+    public class Server : Computer
+    {
+        public string destination;
+        public Server(string make, string dest): base(make, dest)
+        {
+            this.destination = dest;
+        }
+
+    }
+}
